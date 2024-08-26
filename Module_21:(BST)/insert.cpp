104e6e4d041bf1e1cc2634aa4c899b399c196033
Sayhan Ahmed <sayhan@ewnbd.com>
2024-08-26 18:05:36 +0600

    commited

---
+++ Module_21:(BST)/insert.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* binaryLevelInput(){
+    int val, l, r;
+    Node* root, *temp, *left, *right;
+
+    cin >> val;
+    if(val == -1) root = NULL;
+    else root = new Node(val);
+
+    queue<Node*> q;
+    if(root) q.push(root);
+
+    while (!q.empty())
+    {
+        temp = q.front();
+        q.pop();
+
+        cin >> l >> r;
+        if(l == -1) left = NULL;
+        else left = new Node(l);
+        if(r == -1) right = NULL;
+        else right = new Node(r);
+
+        temp->left = left;
+        temp->right = right;
+
+        if(temp->left) q.push(temp->left);
+        if(temp->right) q.push(temp->right);
+    }
+    return root;
+}
+
+void binaryLevelOutput(Node* root){
+    if(root == NULL) return;
+    queue<Node*> q;
+    if(root) q.push(root);
+
+    while (!q.empty())
+    {
+        Node* temp = q.front();
+        q.pop();
+
+        cout << temp->val << " ";
+
+        if(temp->left) q.push(temp->left);
+        if(temp->right) q.push(temp->right);
+    }
+    
+}
+
+Node* insert(Node* &root, int val){
+    Node* newNode = new Node(val);
+    
+    if(root == NULL) {
+        return newNode;
+    }
+
+    if(root->val > val) root->left = insert(root->left, val);
+    else root->right = insert(root->right, val);
+
+    return root;
+}
+
+int main(){
+    Node* root = binaryLevelInput();
+    int  val;
+    cin >> val;
+    
+    insert(root, val);
+    binaryLevelOutput(root);
+    
+    return 0;
+}
